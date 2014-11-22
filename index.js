103be8b7e511b4563a1f3a5781ffb32d5f0b83c2
Helder Santana <helder.css@gmail.com>
2014-11-22 01:26:25 -0500

    let smosh touch vinyl files

--- index.js
+++ index.js
@@ -1,69 +1,68 @@
 'use strict';
 var gutil   = require('gulp-util'),
-	through = require('through2'),
-	smosh   = require('smosh'),
-	verbose = false;
+    through = require('through2'),
+    smosh   = require('smosh'),
+    verbose = false,
+    file    = null;
 
 function verboseMode(file, data) {
-	if (file.path) {
-		gutil.log(file.path);
-	}
+    if (file.path) {
+        gutil.log(file.path);
+    }
 
-	if (data) {
-		gutil.log('gulp-smushit:', 'Compress rate', '%', data.percent);
-		gutil.log('gulp-smushit:', data.src_size, 'bytes  to  ', data.dest_size, 'bytes');
-	}
+    if (data) {
+        gutil.log('gulp-smushit:', 'Compress rate', '%', data.percent);
+        gutil.log('gulp-smushit:', data.src_size, 'bytes  to  ', data.dest_size, 'bytes');
+    }
 }
 
-function onSmoshEnd(file, cb, contents, data) {
-	if (verbose) {
-		verboseMode(file, data);
-	}
+function onSmoshEnd(cb, file, data) {
+    if (verbose) {
+        verboseMode(file, data);
+    }
 
-	file.contents = contents;
+    /*jshint validthis:true */
+    this.push(file);
 
-	/*jshint validthis:true */
-	this.push(file);
-
-	cb();
+    cb();
 }
 
-function onSmoshError(file, cb, msg) {
-	if (msg !== "No savings") {
-		/*jshint validthis:true */
-		return this.emit('error', new gutil.PluginError('gulp-smushit', msg));
-	}
+function onSmoshError(cb, msg, file) {
+    if (msg !== "No savings") {
+        /*jshint validthis:true */
+        return this.emit('error', new gutil.PluginError('gulp-smushit', msg));
+    }
 
-	gutil.log('gulp-smushit:', 'No savings for:', file.path);
+    gutil.log('gulp-smushit:', 'No savings for:', file.path);
 
-	/*jshint validthis:true */
-	this.push(file);
+    /*jshint validthis:true */
+    this.push(file);
 
-	cb();
+    cb();
 }
 
 module.exports = function (options) {
-	if (options && options.verbose) {
-		verbose = true;
-	}
+    if (options && options.verbose) {
+        verbose = true;
+    }
 
-	return through.obj(function (file, enc, cb) {
-		var newFile = null;
+    return through.obj(function (file, enc, cb) {
+        var newFile = null;
 
-		if (file.isNull()) {
-			this.push(file);
-			cb();
-			return;
-		}
+        if (file.isNull()) {
+            this.push(file);
+            cb();
+            return;
+        }
 
-		if (file.isStream()) {
-			this.emit('error', new gutil.PluginError('gulp-smushit', 'Streaming not supported'));
-			cb();
-			return;
-		}
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError('gulp-smushit', 'Streaming not supported'));
+            cb();
+            return;
+        }
 
-		smosh(file.contents)
-			.on('end', onSmoshEnd.bind(this, file, cb))
-			.on('error', onSmoshError.bind(this, file, cb));
-	});
+        smosh(file)
+            .on('end', onSmoshEnd.bind(this, cb))
+            .on('error', onSmoshError.bind(this, cb));
+    });
 };
--- test.js
+++ test.js
@@ -1,61 +1,61 @@
 'use strict';
-var assert 		= require('assert'),
-	gutil 		= require('gulp-util'),
-	smushit 	= require('./'),
-	path 		= require('path'),
-	fs 			= require('fs'),
-	JPG 		= fs.readFileSync(path.join(__dirname, './test_data/fixtures/dp.jpg'), 'binary'),
-	PNG 		= fs.readFileSync(path.join(__dirname, './test_data/fixtures/dp.png'), 'binary'),
-	expectedJPG = fs.readFileSync(path.join(__dirname, './test_data/expected/dp.jpg'), 'binary'),
-	expectedPNG = fs.readFileSync(path.join(__dirname, './test_data/expected/dp.png'), 'binary');
+var assert      = require('assert'),
+    gutil       = require('gulp-util'),
+    smushit     = require('./'),
+    path        = require('path'),
+    fs          = require('fs'),
+    JPG         = fs.readFileSync(path.join(__dirname, './test_data/fixtures/dp.jpg'), 'binary'),
+    PNG         = fs.readFileSync(path.join(__dirname, './test_data/fixtures/dp.png'), 'binary'),
+    expectedJPG = fs.readFileSync(path.join(__dirname, './test_data/expected/dp.jpg'), 'binary'),
+    expectedPNG = fs.readFileSync(path.join(__dirname, './test_data/expected/dp.png'), 'binary');
 
 it('should optimize JPG', function (cb) {
-	var stream = smushit();
+    var stream = smushit();
 
-	stream.on('data', function (file) {
-		assert.equal(file.contents.toString('binary'), expectedJPG);
-	});
+    stream.on('data', function (file) {
+        assert.equal(file.contents.toString('binary'), expectedJPG);
+    });
 
-	stream.on('end', cb);
+    stream.on('end', cb);
 
-	stream.write(new gutil.File({
-		path: path.join(__dirname, './test_data/fixtures/dp.jpg'),
-		contents: new Buffer(JPG, 'binary')
-	}));
+    stream.write(new gutil.File({
+        path: path.join(__dirname, './test_data/fixtures/dp.jpg'),
+        contents: new Buffer(JPG, 'binary')
+    }));
 
-	stream.end();
+    stream.end();
 });
 
 it('should optimize PNG', function (cb) {
-	var stream = smushit({verbose:true});
+    var stream = smushit({verbose:true});
 
-	stream.on('data', function (file) {
-		assert.equal(file.contents.toString('binary'), expectedPNG);
-	});
+    stream.on('data', function (file) {
+        assert.equal(file.contents.toString('binary'), expectedPNG);
+    });
 
-	stream.on('end', cb);
+    stream.on('end', cb);
 
-	stream.write(new gutil.File({
-		path: path.join(__dirname, './test_data/fixtures/dp.png'),
-		contents: new Buffer(PNG, 'binary')
-	}));
+    stream.write(new gutil.File({
+        path: path.join(__dirname, './test_data/fixtures/dp.png'),
+        contents: new Buffer(PNG, 'binary')
+    }));
 
-	stream.end();
+    stream.end();
 });
 
 it('should not break when bytes are not saved', function (cb) {
-	var stream = smushit({verbose:true});
+    var stream = smushit({verbose:true});
 
-	stream.on('data', function (file) {
-		assert.equal(file.contents.toString('binary'), expectedPNG);
-	});
+    stream.on('data', function (file) {
+        assert.equal(file.contents.toString('binary'), expectedPNG);
+    });
 
-	stream.on('end', cb);
+    stream.on('end', cb);
 
-	stream.write(new gutil.File({
-		path: path.join(__dirname, './test_data/expected/dp.png'),
-		contents: new Buffer(expectedPNG, 'binary')
-	}));
+    stream.write(new gutil.File({
+        path: path.join(__dirname, './test_data/expected/dp.png'),
+        contents: new Buffer(expectedPNG, 'binary')
+    }));
 
-	stream.end();
+    stream.end();
 });
